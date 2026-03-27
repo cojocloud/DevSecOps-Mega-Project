746a130858486b693973004cec505c7a4062893f
cojocloud <cojocloudsolutions@gmail.com>
2026-03-26 21:31:03 -0400

    updated instance ids in the scripts

--- Automations/updatefrontendnew.sh
+++ Automations/updatefrontendnew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the Instance ID and path to the .env file
-INSTANCE_ID="i-0e29eb354a5ea9275"
+INSTANCE_ID="i-0ea7a46b6c5f2a5b8"
 
 # Retrieve the public IP address of the specified EC2 instance
 ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
